16ab0a66c7950bfa63a63c9bfdcf096a08ea2327
ant <ant@ant.ant>
2023-12-07 21:25:05 +0100

    .form

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ def get():
 
 @app.route("/post", methods=["POST"])
 def post():
-    print(request)
+    print(request.form)
     return {"msg":f"bonjour {request.form['name']}"}
 
 app.run(host="0.0.0.0")
