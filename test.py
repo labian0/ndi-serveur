299b53e46ffb4aa99059fccb8833fafa01315372
ant <ant@ant.ant>
2023-12-08 07:19:42 +0100

    final

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 json = requests.post("http://91.92.252.31:5000/login", data={"username": "apagnan", "password": "apagnan"}).json()
-r = requests.post("http://91.92.252.31:5000/init_plateau", data=json)
+r = requests.post("http://91.92.252.31:5000/actions_disponibles", data=json)
 print(r.status_code)
 game = r.json()
 print(game)
